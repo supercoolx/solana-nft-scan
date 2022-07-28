905c6ac29544b9fc5fc3f1932bdf53441cd566d4
supercoolx <marco.wong.sg@gmail.com>
2022-07-28 10:21:39 -0700

    bug

--- index1.js
+++ index1.js
@@ -3,7 +3,7 @@ import axios from "axios";
 
 const main = async () => {
     var i;
-    for (i = 0; i < 100; i += 25) {
+    for (i = 0; i < 9500; i += 25) {
         let output = "";
         console.log(i);
         const { data: result } = await axios.get(`https://api.solscan.io/collection/nft?sortBy=nameDec&collectionId=40b023488409e7c9093a1729f2738991fdcbd60e23aec5d41642239477ad345f&offset=${i}&limit=25`);
