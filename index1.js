7a1d22978a7f195125d65d13e693110755568bac
supercoolx <marco.wong.sg@gmail.com>
2022-07-28 10:29:02 -0700

    try catch

--- index1.js
+++ index1.js
@@ -1,16 +1,27 @@
 import fs from "fs";
 import axios from "axios";
 
+const sleep = async () => {
+    const promise = new Promise(resolve => setTimeout(resolve, 3000));
+    await promise();
+}
+
 const main = async () => {
     var i;
     for (i = 0; i < 9500; i += 25) {
         let output = "";
         console.log(i);
-        const { data: result } = await axios.get(`https://api.solscan.io/collection/nft?sortBy=nameDec&collectionId=40b023488409e7c9093a1729f2738991fdcbd60e23aec5d41642239477ad345f&offset=${i}&limit=25`);
-        result.data.forEach(token => {
-            output += token.info.mint + "\n";
-        });
-        fs.appendFileSync('output.json', output);
+        try {
+            const { data: result } = await axios.get(`https://api.solscan.io/collection/nft?sortBy=nameDec&collectionId=40b023488409e7c9093a1729f2738991fdcbd60e23aec5d41642239477ad345f&offset=${i}&limit=25`);
+            result.data.forEach(token => {
+                output += token.info.mint + "\n";
+            });
+            fs.appendFileSync('output.json', output);
+        }
+        catch(err) {
+            i -= 25;
+            await sleep();
+        }
     }
 }
 
