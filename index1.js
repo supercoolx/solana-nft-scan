aa618d8f2981771484c06038eef6cbfb21b7c09c
supercoolx <marco.wong.sg@gmail.com>
2022-07-30 12:31:32 -0700

    .

--- index1.js
+++ index1.js
@@ -4,14 +4,13 @@ const axios = require("axios");
 const sleep = () => new Promise(resolve => setTimeout(resolve, 3000));
 
 const main = async () => {
-    fs.writeFileSync('output1.txt', "");
+    // fs.writeFileSync('output1.txt', "");
     var i;
-    for (i = 0; ; i += 25) {
+    for (i = 7125; i < 9500 ; i += 25) {
         let output = "";
         console.log(i);
         try {
             const { data: result } = await axios.get(`https://api.solscan.io/collection/nft?sortBy=nameDec&collectionId=40b023488409e7c9093a1729f2738991fdcbd60e23aec5d41642239477ad345f&offset=${i}&limit=25`);
-            if (result.data.length === 0) break;
             result.data.forEach(token => {
                 if (token.info.updateAuthority === "BRAiNSRTXfk6Rx95FAfsQVAfDctgqNR6PrYAxsa3sjhv")
                     output += token.info.mint + "\n";
