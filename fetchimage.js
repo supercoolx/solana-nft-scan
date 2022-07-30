97999ecfedee8d18a42751d265eb64cb3fc7b918
supercoolx <marco.wong.sg@gmail.com>
2022-07-29 18:38:10 -0700

    time

--- fetchimage.js
+++ fetchimage.js
@@ -4,14 +4,17 @@ const path = require('path');
 const download = require("download");
 const urls = require("./imageurls.json");
 
-const sleep = () => new Promise(resolve => setTimeout(resolve, 3000));
+const sleep = () => new Promise(resolve => setTimeout(resolve, 10000));
 
 const downloadImage = async (url) => {
     try {
         const filename = path.basename(URL.parse(url).pathname);
         console.log(filename);
         if (fs.existsSync(path.join('assets', filename))) return;
-        await download(url, 'assets');
+        await Promise.all([
+            sleep(),
+            download(url, 'assets')
+        ]);
     } catch (err) {
         fs.appendFileSync('error.log', url + '\n');
     }
